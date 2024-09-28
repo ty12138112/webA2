bd0a3cbef19333e88c91272ab00c667443a7ba78
ty12138112 <2118526697@qq.com>
2024-09-28 22:48:27 +0800

    Create GET method to retrieve all active fundraisers including the category based on criteria  from the database.

--- API/controllerAPI/api-controller.js
+++ API/controllerAPI/api-controller.js
@@ -8,27 +8,60 @@ var express = require('express');
 
 var router = express.Router();
 
-router.get("/fundraisers", (req, res)=>{
-	connection.query("SELECT f.FUNDRAISER_ID, f.ORGANIZER, f.CAPTION, f.TARGET_FUNDING, f.CURRENT_FUNDING, f.CITY, c.NAME AS CATEGORY_NAME FROM FUNDRAISER f JOIN CATEGORY c ON f.CATEGORY_ID = c.CATEGORY_ID WHERE f.ACTIVE = TRUE", (err, records,fields)=> {
-		 if (err){
-			 console.error("Error while retrieve the data");
-		 }else{
-			 res.send(records);
-		 }
+router.get("/fundraisers", (req, res) => {
+	connection.query("SELECT f.FUNDRAISER_ID, f.ORGANIZER, f.CAPTION, f.TARGET_FUNDING, f.CURRENT_FUNDING, f.CITY, c.NAME AS CATEGORY_NAME FROM FUNDRAISER f JOIN CATEGORY c ON f.CATEGORY_ID = c.CATEGORY_ID WHERE f.ACTIVE = TRUE", (err, records, fields) => {
+		if (err) {
+			console.error("Error while retrieve the data");
+		} else {
+			res.send(records);
+		}
 	})
 })
 
-router.get("/categories", (req, res)=>{
-	connection.query("SELECT * FROM CATEGORY", (err, records,fields)=> {
-		 if (err){
-			 console.error("Error while retrieve the data");
-		 }else{
-			 res.send(records);
-		 }
+router.get("/categories", (req, res) => {
+	connection.query("SELECT * FROM CATEGORY", (err, records, fields) => {
+		if (err) {
+			console.error("Error while retrieve the data");
+		} else {
+			res.send(records);
+		}
 	})
 })
 
-
+router.get('/search', (req, res) => {  
+    let sql = 'SELECT f.FUNDRAISER_ID, f.ORGANIZER, f.CAPTION, f.TARGET_FUNDING, f.CURRENT_FUNDING, f.CITY, c.NAME AS CATEGORY_NAME FROM FUNDRAISER f INNER JOIN CATEGORY c ON f.CATEGORY_ID = c.CATEGORY_ID WHERE f.ACTIVE = 1';  
+      
+    let params = [];  
+  
+    if (req.query.organizer) {  
+        sql += ' AND f.ORGANIZER LIKE ?';  
+        params.push(`%${req.query.organizer}%`);  
+    }  
+    if (req.query.city) {  
+        sql += ' AND f.CITY LIKE ?';  
+        params.push(`%${req.query.city}%`);  
+    }  
+    if (req.query.category) {  
+        sql += ' AND c.NAME LIKE ?';   
+        params.push(`%${req.query.category}%`);  
+    }  
+  
+    connection.query(sql, params, (err, records, fields) => {  
+        if (err) {  
+            console.error("Error while retrieving the data:"); 
+        } else {  
+            res.send(records);  
+        }  
+    });  
+});
+router.get('/api/fundraiser/:id', (req, res) => {
+	const fundraiserId = req.params.id;
+	const sql = 'SELECT f.FUNDRAISER_ID, f.ORGANIZER, f.CAPTION, f.TARGET_FUNDING, f.CURRENT_FUNDING, f.CITY, c.NAME AS CATEGORY_NAME FROM FUNDRAISER f INNER JOIN CATEGORY c ON f.CATEGORY_ID = c.CATEGORY_ID WHERE f.FUNDRAISER_ID = ? AND f.ACTIVE = 1';
+	connection.query(sql, [fundraiserId], (err, results) => {
+		if (err) throw err;
+		res.json(results);
+	});
+});
 
 // router.get("/:id", (req, res)=>{
 // 	connection.query("select * from concert where ConcertID=" + req.params.id, (err, records,fields)=> {
